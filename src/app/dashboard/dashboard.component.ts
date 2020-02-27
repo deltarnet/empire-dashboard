51d74ac46c5de9bd7d0d7e9e7b867c972af3df51
Dilyen <davidandohacquah@gmail.com>
2020-02-27 18:41:06 +0000

    made radio dynamic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { ViewEncapsulation } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
+
+export interface StatusData {
+    project: string;
+    status: number;
+    endpoint: string;
+    requestmethod: string;
+}
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['../app.component.scss','./dashboard.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: [
+        '../app.component.scss', './dashboard.component.scss'
+    ],
+    encapsulation: ViewEncapsulation.None
 })
+
+
 export class DashboardComponent implements OnInit {
+  retrieved_data: StatusData[] = [
+    {
+        project: "Time Entry System",
+        status: 200,
+        endpoint: "https://projectervice002.herokuapp.com/v1/api/employee/",
+        requestmethod: "GET"
+    }, {
+        project: "Pollster",
+        status: 500,
+        endpoint: "https://employeeservice002.herokuapp.com/v1/api/employees",
+        requestmethod: ""
+    }
+]; 
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor() {}
+
+    ngOnInit() {}
+    
 
 }
