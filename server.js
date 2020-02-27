b8ba75105e25df5ede9f660e3a7e5917e3a04e5f
Dilyen <davidandohacquah@gmail.com>
2020-02-27 19:21:30 +0000

    removed some config files

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const cors = require('cors');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/empire-dashboard'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/empire-dashboard/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface StatusData {
+  project: string;
+  status: number;
+  endpoint: string;
+  requestmethod: string;
+}
+
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.scss']
 })
 export class StatusComponent implements OnInit {
+  retrieved_data: StatusData[] = [
+    {
+        project: "Time Entry System",
+        status: 200,
+        endpoint: "https://projectervice002.herokuapp.com/v1/api/employee/",
+        requestmethod: "GET"
+    }, {
+        project: "Pollster",
+        status: 500,
+        endpoint: "https://employeeservice002.herokuapp.com/v1/api/employees",
+        requestmethod: ""
+    }
+]; 
 
   constructor() { }
 
