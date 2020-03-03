219e42ff1fb41cebaa1f5c8d35b6cf91bf642d7e
Dilyen <davidandohacquah@gmail.com>
2020-03-03 10:26:15 +0000

    creating post form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import {StatusComponent} from './status/status.component';
-import {FormComponent} from './form/form.component';
+import {
+    NgModule
+}
+from '@angular/core';
+import {
+    RouterModule, Routes
+}
+from '@angular/router';
+import {
+    DashboardComponent
+}
+from './dashboard/dashboard.component';
+import {
+    StatusComponent
+}
+from './status/status.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  {path: 'status', component: StatusComponent},
-  {path: 'form', component: FormComponent},
+const routes: Routes = [{ path: '', redirectTo: '/dashboard', pathMatch: 'full' }, { path: 'dashboard', component: DashboardComponent }, {path: 'status', component: StatusComponent}, ];
 
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] }) export class AppRoutingModule {}
---
+++ src/app/endpoints.ts
@@ -0,0 +1,8 @@
+export class Endpoints {
+    name: string
+    endpoint_urls: EndpointUrl[]
+}
+
+export class EndpointUrl{
+    endpoint_url: string
+}
