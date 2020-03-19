7e41d13801d5737a2fd2473573462b94048b7fd3
Dilyen <davidandohacquah@gmail.com>
2020-03-19 14:54:01 +0000

    add form reset & value summit working

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,30 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
-@Component({
-  template: '',
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+@Component({template: '', selector: 'app-form', templateUrl: './form.component.html', styleUrls: ['./form.component.scss']})export class FormComponent implements OnInit {
 
-}) export class FormComponent implements OnInit {
+    public projects : any[] = [{
+            project: ''
+        }];
 
-    public projects: any[] = [{ project: '' }];
-
-    public endpoints: any[] = [{ endpoints: '' }];
+    public endpoints : any[] = [{
+            endpoints: ''
+        }];
 
     constructor() {}
 
     ngOnInit() {}
 
     addProject() {
-        this.projects.push({ project: '' });
+        this.projects.push({project: ''});
     }
     addEndpoint() {
-        this.endpoints.push({ endpoint: '' });
+        this.endpoints.push({endpoint: ''});
     }
 
-    logValue() {
-      console.log(this.projects);
-      console.log(this.endpoints);
+    logValue(form : NgForm) {
+        // const value = projName.value;
+        // console.log(value)
+        console.log(form.value)
+        form.reset()
+        // console.log(value.projectName)
+
+
+        // console.log(this.projects);
+        // console.log(this.endpoints);
     }
 }
